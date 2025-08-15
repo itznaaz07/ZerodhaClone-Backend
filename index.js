5a00ba20952c5722ff3f95c328d01b90d7bd868b
Ganiya naaz <ganiyanaaz@gmail.com>
2025-08-15 19:51:18 +0530

    Fix CORS for cookies

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -31,7 +32,6 @@ app.use(cors({
   },
   credentials: true
 }));
-const app = express();
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use("/api", authRoutes); 
